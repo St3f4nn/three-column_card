fe2b0b033d927ae22cda64d3999b18f7058f44b1
Stefan Gajić <stefan.gajic@nenasal.com>
2022-04-07 20:58:48 +0200

    Adding keywords and description, adding styles and designing layout and finializing project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,39 @@ module.exports = {
     './dist/*.html'
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '280px',
+      base: '640px',
+      md: '768px',
+      lg: '1155px',
+      xl: '1295px'
+    },
+    extend: {
+      colors: {
+        e38826: '#e38826',
+        zero06970: '#006970',
+        zero0403f: '#00403f',
+        transparentWhite: 'hsla(0, 0%, 100%, 0.75)',
+        f2f2f2: '#f2f2f2'
+      },
+      fontFamily: {
+        bigShouldersDisplay: "'Big Shoulders Display', cursive",
+        lexendDeca: "'Lexend Deca', sans-serif"
+      },
+      spacing: {
+        18: '18px',
+        50: '50px',
+        104: '104px',
+        160: '160px',
+        230: '230px'
+      },
+      fontSize: {
+        15: '15px'
+      },
+      maxWidth: {
+        210: '210px'
+      }
+    },
   },
   plugins: [],
 }
